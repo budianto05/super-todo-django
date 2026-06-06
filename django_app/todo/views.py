636754acabbe0ade1eccc5df7a4b002e2c10529a
budianto05 <antobudi4600@gmail.com>
2026-06-07 00:38:57 +0800

    Improve due date parsing and fix todos query
    Updated the due date handling to safely parse datetime strings and corrected the todos query.

--- django_app/todo/views.py
+++ django_app/todo/views.py
@@ -1,35 +1,47 @@
-from django.shortcuts import render, redirect, get_object_address, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
-from django.utils import timezone
+from django.utils.dateparse import parse_datetime
 
+# READ + CREATE
 def todo_list(request):
-    # create/tulis
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
         priority = request.POST.get('priority', 'M')
-        due_date = request.POST.get('due_date')
-        
+        due_date_str = request.POST.get('due_date')
+
+        # parse string to datetime safely
+        due_date = parse_datetime(due_date_str) if due_date_str else None
+
         Todo.objects.create(
             title=title,
             description=description,
             priority=priority,
-            due_date=due_date if due_date else None
+            due_date=due_date
         )
 
         return redirect('todo_list')
-    # read/baca
-    todo = Todo_objects.all().order_by('is_completed', '-priority', 'due_date')
-    return render(request, 'todo/todo_list.html', {'todos': todos})
 
-# update/edit
-def todo_toggel(request, pk):
+    todos = Todo.objects.all().order_by(
+        'is_completed',
+        '-priority',
+        'due_date'
+    )
+
+    return render(
+        request,
+        'todo/todo_list.html',
+        {'todos': todos}
+    )
+
+# UPDATE / TOGGLE
+def todo_toggle(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.is_completed = not todo.is_completed
     todo.save()
     return redirect('todo_list')
 
-# delete/hapus
+# DELETE
 def todo_delete(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.delete()
